76637cd1e9a873f4c9ee18fee0a3609ffa1ed301
Matt Majestic <33904170+mattmajestic@users.noreply.github.com>
2024-03-15 16:15:54 -0400

    add gorm and tmpl

--- go.mod
+++ go.mod
@@ -13,6 +13,8 @@ require (
 	github.com/go-playground/universal-translator v0.17.0 // indirect
 	github.com/go-playground/validator/v10 v10.4.1 // indirect
 	github.com/golang/protobuf v1.3.3 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/json-iterator/go v1.1.9 // indirect
 	github.com/leodido/go-urn v1.2.0 // indirect
 	github.com/mattn/go-isatty v0.0.12 // indirect
@@ -22,4 +24,5 @@ require (
 	golang.org/x/crypto v0.21.0 // indirect
 	golang.org/x/sys v0.18.0 // indirect
 	gopkg.in/yaml.v2 v2.2.8 // indirect
+	gorm.io/gorm v1.25.7 // indirect
 )
--- main.go
+++ main.go
@@ -2,110 +2,36 @@ package main
 
 import (
     "github.com/gin-gonic/gin"
-    // "encoding/json"
-    // "fmt"
-    // "io/ioutil"
-    // "log"
-    // "net/http"
-    // "os"
-
-    // "github.com/joho/godotenv"
+    "gorm.io/driver/sqlite"
+    "gorm.io/gorm"
+    "net/http"
 )
 
-/*
-type Video struct {
-    Id string `json:"videoId"`
+type User struct {
+    gorm.Model
+    Name string
 }
-
-type Item struct {
-    Id Video `json:"id"`
-    Statistics struct {
-        ViewCount    string `json:"viewCount"`
-        LikeCount    string `json:"likeCount"`
-        CommentCount string `json:"commentCount"`
-    } `json:"statistics"`
-}
-
-type Response struct {
-    Items []Item `json:"items"`
-}
-
-func getYoutubeMetrics(c *gin.Context) {
-    err := godotenv.Load()
-    if err != nil {
-        log.Fatal("Error loading .env file")
-    }
-
-    API_KEY := os.Getenv("YT_API_KEY")
-    if API_KEY == "" {
-        c.JSON(http.StatusBadRequest, gin.H{"error": "YouTube API key not found"})
-        return
-    }
-
-    CHANNEL_ID := "UCjTavL86-CW6j58fsVIjTig"
-    url := fmt.Sprintf("https://www.googleapis.com/youtube/v3/search?key=%s&channelId=%s&part=id&maxResults=100", API_KEY, CHANNEL_ID)
-    resp, err := http.Get(url)
-    if err != nil {
-        log.Printf("Error fetching YouTube search: %v", err)
-        c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to fetch data from YouTube"})
-        return
-    }
-    defer resp.Body.Close()
-
-    body, err := ioutil.ReadAll(resp.Body)
-    if err != nil {
-        log.Printf("Error reading response body: %v", err)
-        c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to read response from YouTube"})
-        return
-    }
-
-    var searchData Response
-    json.Unmarshal(body, &searchData)
-
-    videoData := make([]map[string]string, 0)
-
-    for _, item := range searchData.Items {
-        videoID := item.Id.Id
-        statsUrl := fmt.Sprintf("https://www.googleapis.com/youtube/v3/videos?key=%s&id=%s&part=statistics", API_KEY, videoID)
-        statsResp, err := http.Get(statsUrl)
-        if err != nil {
-            log.Printf("Error fetching YouTube video statistics: %v", err)
-            continue
-        }
-        defer statsResp.Body.Close()
-
-        statsBody, err := ioutil.ReadAll(statsResp.Body)
-        if err != nil {
-            log.Printf("Error reading statistics response body: %v", err)
-            continue
-        }
-
-        var statsData Response
-        json.Unmarshal(statsBody, &statsData)
-
-        for _, statsItem := range statsData.Items {
-            videoMetrics := map[string]string{
-                "Video ID":  videoID,
-                "Views":     statsItem.Statistics.ViewCount,
-                "Likes":     statsItem.Statistics.LikeCount,
-                "Comments":  statsItem.Statistics.CommentCount,
-            }
-
-            videoData = append(videoData, videoMetrics)
-        }
-    }
-
-    c.JSON(http.StatusOK, videoData)
-}
-*/
 
 func main() {
+    db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+    if err != nil {
+        panic("failed to connect database")
+    }
+
+    // Migrate the schema
+    db.AutoMigrate(&User{})
+
     router := gin.Default()
 
-    router.StaticFile("/", "./index.html")
+    router.LoadHTMLGlob("templates/*")
 
-    // Commented out YouTube-related code
-    // router.GET("/youtube-metrics", getYoutubeMetrics)
+    router.GET("/", func(c *gin.Context) {
+        var users []User
+        db.Find(&users)
+        c.HTML(http.StatusOK, "index.tmpl", gin.H{
+            "users": users,
+        })
+    })
 
     router.Run(":8080")
 }
